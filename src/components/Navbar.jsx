9f4342b0e58940108fd08e37d88ab2576940dfc0
katjaferreri <katjadanielaferreri@gmail.com>
2022-08-22 13:58:25 -0300

    primer commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Form from "./Form"
+import cartWidget from "./cartWidget";
+
+const Navbar = () => {
+  return (
+    <div>
+<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarColor01">
+      <ul className="navbar-nav me-auto">
+        <li className="nav-item">
+          <a className="nav-link active" href="#">Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Features</a>
+        </li>
+        {/*
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+          <div className="dropdown-menu">
+            <a className="dropdown-item" href="#">Action</a>
+            <a className="dropdown-item" href="#">Another action</a>
+            <a className="dropdown-item" href="#">Something else here</a>
+            <div className="dropdown-divider" />
+            <a className="dropdown-item" href="#">Separated link</a>
+          </div>
+        </li>*/}
+        </ul>
+        <cartWidget/>
+        <Form contBuscar="Productos"/>
+    </div>
+  </div>
+</nav>
+    </div>
+  );
+}
+
+export default Navbar;
